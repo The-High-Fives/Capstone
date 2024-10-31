5da56d13b3de6e866497a0ad7d9be4632bf26244
brianhz1 <131827063+brianhz1@users.noreply.github.com>
2024-10-31 01:43:31 -0500

    updated buffer interface

--- src/hardware/rtl/cpu/ex_m_buffer.sv
+++ src/hardware/rtl/cpu/ex_m_buffer.sv
@@ -10,8 +10,12 @@ module ex_m_buffer
     // memory
     input ex_MemWrite,
     input ex_MemRead,
+    input ex_JAL,
+    input ex_LUI,
     output logic m_MemWrite,
     output logic m_MemRead,
+    output logic m_JAL,
+    output logic m_LUI,
 
     input [4:0] ex_rs2,
     input [4:0] ex_rd,
@@ -21,14 +25,20 @@ module ex_m_buffer
     // datapath
     input [31:0] ex_alu_out,
     input [31:0] ex_mem_data,
+    input [31:0] ex_sext_out,
+    input [31:0] ex_pc_inc_out,
     output logic [31:0] m_alu_out,
     output logic [31:0] m_mem_data
+    output logic [31:0] m_sext_out,
+    output logic [31:0] m_pc_inc_out,
 );
 
     always_ff @(posedge clk, negedge rst_n) begin
         if (!rst_n) begin
             m_MemToReg <= 0;
             m_RegWrite <= 0;
+            m_JAL <= 0;
+            m_LUI <= 0;
             m_MemWrite <= 0;
             m_MemRead <= 0;
             m_rs2 <= 0;
@@ -39,6 +49,8 @@ module ex_m_buffer
         else begin
             m_MemToReg <= ex_MemToReg;
             m_RegWrite <= ex_RegWrite;
+            m_JAL = ex_JAL;
+            m_LUI = ex_LUI;
             m_MemWrite <= ex_MemWrite;
             m_MemRead <= ex_MemRead;
             m_rs2 <= ex_rs2;
--- src/hardware/rtl/cpu/mem_wb_buffer.sv
+++ src/hardware/rtl/cpu/mem_wb_buffer.sv
@@ -7,27 +7,33 @@ module mem_wb_buffer (
     input logic m_RegWrite,       
 
     // Data from MEM stage
-    input logic [31:0] m_alu_out,      
-    input logic [31:0] m_mem_data, // Data read from memory
+    input logic [31:0] m_read_data,      
+    input logic [31:0] m_reg_data, // Data read from memory
     input logic [4:0] m_rd,
 
     // Forwarded data to WB stage
-    output logic [31:0] wb_data, // Data to write back to register
+    output logic [31:0] wb_read_data, // Data to write back to register
+    output logic [31:0] wb_reg_data,
     output logic [4:0] wb_rd,          
-    output logic wb_RegWrite // Write enable for register in WB stage
+    output logic wb_RegWrite, // Write enable for register in WB stage
+    output logic wb_MemToReg
 );
 
     always_ff @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            wb_data <= 32'd0;
+            wb_read_data <= 32'd0;
+            wb_reg_data <= 32'd0;
             wb_rd <= 5'd0;
             wb_RegWrite <= 1'b0;
+            wb_MemToReg <= 1'b0
         end 
         else begin
             // Select either memory or ALU result based on MemToReg control signal
-            wb_data <= m_MemToReg ? m_mem_data : m_alu_out;
+            wb_read_data <= m_read_data;
+            wb_reg_data <= m_reg_data;
             wb_rd <= m_rd;
             wb_RegWrite <= m_RegWrite;
+            wb_MemToReg <= m_MemToReg;
         end
     end
 
