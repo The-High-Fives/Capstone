0e5989b9ed74865945cccd4f0bc58c7f87071a2a
brianhz1 <131827063+brianhz1@users.noreply.github.com>
2024-11-14 12:37:04 -0600

    fixed depth parameter

--- src/hardware/rtl/cpu/dmem32.sv
+++ src/hardware/rtl/cpu/dmem32.sv
@@ -1,6 +1,7 @@
+// max size of 32768 memory locations
 module dmem32
 #(
-    parameter depth = 16383 // 128K = 32768 32-bit memory location, 64K = 16384 32-bit memory location 
+    parameter depth = 16384 // 128K = 32768 32-bit memory location, 64K = 16384 32-bit memory location 
 )
 (
     // inputs
@@ -16,12 +17,12 @@ module dmem32
 );
 
     // memory, each location stores a 32-bit word (4 bytes), each bank store 1 byte
-    reg [7:0] mem [0:depth];
+    reg [7:0] mem [0:depth-1];
 
     // Read operation
     always_ff @(posedge clk) begin // rst_n
         if (re) 
-            rdata <= mem[addr]; // Read data from memory at the given address
+            rdata <= mem[addr[$clog2(depth)-1:0]]; // Read data from memory at the given address
     end
 
     // Write operation
