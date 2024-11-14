9e454d9fd033af8f537ccebf9983607075eb74bd
brianhz1 <131827063+brianhz1@users.noreply.github.com>
2024-11-14 17:22:15 -0600

    changed to 13 bit address

--- src/hardware/rtl/cpu/dmem32.sv
+++ src/hardware/rtl/cpu/dmem32.sv
@@ -8,7 +8,7 @@ module dmem32
     // inputs
     input logic clk,
     input logic rst_n, 
-    input logic [14:0] addr,  // 15-bit address for 32KB (128KB memory)
+    input logic [13:0] addr,  // 15-bit address for 32KB (128KB memory)
     input logic re,           // Read enable
     input logic we,           // Write enable
     input logic [7:0] wdata, // 32-bit data to be written into memory
@@ -20,21 +20,17 @@ module dmem32
     // memory, each location stores a 32-bit word (4 bytes), each bank store 1 byte
     reg [7:0] mem [0:depth-1];
 
-    // Read operation
-    always_ff @(posedge clk) begin // rst_n
-        if (re) 
-            rdata <= mem[addr[$clog2(depth)-1:0]]; // Read data from memory at the given address
-    end
-
     // Write operation
     always_ff @(posedge clk) begin // rst_n
         if (we) 
             mem[addr] <= wdata; // Write to memory during normal operation
+        // if (re)
+        rdata <= mem[addr]; // Read data from memory at the given address
     end
 
     // Reading memory
-    // initial begin
-    //     $readmemh(FILENAME, mem);
-    // end
+    initial begin
+        $readmemh(FILENAME, mem);
+    end
 
 endmodule
