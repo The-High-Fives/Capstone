5497544a770d0090656ca99081013cb81dada5a0
Rusheel <rusheel@caredocs.ai>
2024-11-17 07:49:11 -0600

    group detection and IPU files created

---
+++ src/hardware/img_proc/group_detection.sv
@@ -0,0 +1,14 @@
+module group_detection (
+    input iColor,
+    input iDVAL,
+    input iX_Cont,
+    input iY_Cont,
+    output oX,
+    output oY,
+    output oDVAL,
+    input iCLK,
+    input iRST
+);
+
+
+endmodule
