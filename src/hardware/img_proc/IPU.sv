86369abc4906c70fd548cd4ec23ec55e199bcbf6
Rusheel <rusheel@caredocs.ai>
2024-11-21 16:26:10 -0600

    ipu testbench expanded slightly

--- src/hardware/img_proc/IPU.sv
+++ src/hardware/img_proc/IPU.sv
@@ -3,14 +3,48 @@ module IPU(
     input iCLK;
     input iRST;
     input iDVAL;
-    input [11:0] iDATA;
+    input	[11:0]	iRed,
+    input	[11:0]	iGreen,
+    input	[11:0]	iBlue,
     input [10:0] iX_Cont;
     input [10:0] iY_Cont;
 
     // outputs
-    output [10:0] avg_X;
-    output [10:0] avg_Y;
-    output valid;
+    output [10:0] oX;
+    output [10:0] oY;
+    output oDVAL;
+);
+
+logic [11:0] filterRed;
+logic [11:0] filterGreen;
+logic [11:0] filterBlue;
+logic filterDVAL;
+
+color_filter_unified u_color_filter_unified (
+    .iX_Cont(iX_Cont),
+    .iY_Cont(iY_Cont),
+    .iRed(iRed),
+    .iGreen(iGreen),
+    .iBlue(iBlue),
+    .iDVAL(iDVAL),
+    .iCLK(iCLK),
+    .iRST(iRST),
+    .oRed(filterRed),
+    .oGreen(filterGreen),
+    .oBlue(filterBlue),
+    .oDVAL(filterDVAL)
+);
+
+group_detection u_group_detection (
+    .iX_Cont(iX_Cont),
+    .iY_Cont(iY_Cont),
+    .iColor(filterRed),
+    .iDVAL(filterDVAL),
+    .iCLK(iCLK),
+    .iRST(iRST),
+    .oX(oX),
+    .oY(oY),
+    .oDVAL(oDVAL)
 );
 
 endmodule
--- src/hardware/img_proc/IPU_tb.sv
+++ src/hardware/img_proc/IPU_tb.sv
@@ -3,7 +3,9 @@ module IPU_tb ();
     // inputs
     logic clk, rst;
     logic dVAL;
-    logic [11:0] data;
+    logic	[11:0]	red,
+    logic	[11:0]	greed,
+    logic	[11:0]	blue,
     logic [10:0] iX_Cont;
     logic [10:0] iY_Cont;
 
@@ -11,4 +13,25 @@ module IPU_tb ();
     logic [10:0] avg_X;
     logic [10:0] avg_Y;
     logic valid;
+
+    IPU dut(
+        .iCLK(clk),
+        .iRST(rst),
+        .iDVAL(dVAL),
+        .iRed(red),
+        .iGreen(green),
+        .iBlue(blue),
+        .iX_Cont(iX_Cont),
+        .iY_Cont(iY_Cont),
+        .oX(avg_X),
+        .oY(avg_Y),
+        .oDVAL(valid)
+    );
+
+    initial begin 
+        clk = 0;
+        forever #5 clk = ~clk;
+    end
+    
+
 endmodule
