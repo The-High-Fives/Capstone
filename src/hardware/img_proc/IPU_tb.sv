622f0d3ce610bed9868268532b49fa8f5ea090e3
Rusheel <rusheel@caredocs.ai>
2024-11-21 16:58:14 -0600

    testbench v1?

--- src/hardware/img_proc/IPU_tb.sv
+++ src/hardware/img_proc/IPU_tb.sv
@@ -32,6 +32,30 @@ module IPU_tb ();
         clk = 0;
         forever #5 clk = ~clk;
     end
+    initial begin
+        rst_n = 0;
+        dVAL = 0;
+        red = 0;
+        green = 0;
+        blue = 0;
+        iX_Cont = 0;
+        iY_Cont = 0;
+
+        repeat (10) @(posedge clk);
+        rst_n = 1;
+
+        repeat (480) begin
+            repeat (640) @(posedge clk) begin
+                dVAL = 1;
+                red = 12'hFFF;
+                green = 12'h000;
+                blue = 12'h000;
+                iX_Cont = iX_Cont + 1;
+            end
+            iX_Cont = 0;
+            iY_Cont = iY_Cont + 1;
+        end
+    end
 
 
 endmodule
