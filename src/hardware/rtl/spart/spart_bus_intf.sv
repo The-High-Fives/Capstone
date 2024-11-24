af20a8020ac602096f91a6e34273f164674786d3
brianhz1 <131827063+brianhz1@users.noreply.github.com>
2024-11-24 10:32:33 -0600

    changed address

--- src/hardware/rtl/spart/spart_bus_intf.sv
+++ src/hardware/rtl/spart/spart_bus_intf.sv
@@ -14,7 +14,7 @@ module spart_bus_intf(
     inout [31:0] data_o,
     inout ack_o
 );
-    localparam addr_offset = 30'h10000007; // 0x1C
+    localparam addr_offset = 30'h10000007; // 0x4000001C
 
     wire cs; // device 'cs' enable based on address
     wire [7:0] status_reg;
