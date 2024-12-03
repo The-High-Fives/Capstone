84a234511cb0f58d57b33cacd10c75808627b406
Rusheel <rusheel@caredocs.ai>
2024-12-03 16:39:11 -0600

    new PRU test (it's kinda dynamic

--- src/firmware/tests/pruTest.c
+++ src/firmware/tests/pruTest.c
@@ -2,30 +2,33 @@
 
 int main()
 {
-    drawCircle(300, 100, 30, 1);
-    drawRect(300, 300, 50, 50, 2);
-    // int i, iter = 0;
-    // do
-    // {
-    //     iter = (iter + 1) & 7;
+    int i, iter = 0;
+    do
+    {
+        iter = (iter + 1) & 7;
 
-    //     int x = 80 + (iter & 1) * 160;
-    //     int y = 80 + (iter & 2) * 80;
+        int x = 80 + (iter & 1) * 160;
+        int y = 80 + (iter & 2) * 80;
 
-    //     if (iter & 4)
-    //     {
-    //         drawCircle(x, y, 10, 1);
-    //     }
-    //     else
-    //     {
-    //         drawRect(x, y, 10, 10, 2);
-    //     }
+        if (iter == 4 || iter == 0)
+        {
+            drawRect(0, 0, 640, 480, 0);
+        }
 
-    //     for (i = 0; i < 10000000; i++)
-    //     {
-    //     }
+        if (iter & 4)
+        {
+            drawCircle(x, y, 40 + iter * 10, 1);
+        }
+        else
+        {
+            drawRect(x, y, 50 + iter * 10, 50 + iter * 10, 2);
+        }
 
-    // } while (1);
+        for (i = 0; i < 100000; i++)
+        {
+        }
+
+    } while (1);
 
 end:
     goto end;
