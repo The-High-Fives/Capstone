78a11c1e116fb1030e728cd2a9ceb98528ecffc8
Rusheel <rusheel@caredocs.ai>
2024-12-03 17:20:59 -0600

    slight code change

--- src/firmware/tests/pruTest.c
+++ src/firmware/tests/pruTest.c
@@ -7,21 +7,21 @@ int main()
     {
         iter = (iter + 1) & 31;
 
-        int x = 80 + (iter & 3) * 160;
-        int y = 80 + (iter & 12) * 40;
+        int x = 80 + (iter & 3) * 80;
+        int y = 80 + (iter & 12) * 20;
 
         if (iter == 16 || iter == 0)
         {
-            drawRect(0, 0, 640, 480, 0);
+            drawRect(0, 0, 0x3FF, 0x1FF, iter == 16 ? 3 : 0);
         }
 
-        if (iter & 16)
+        if (iter < 16)
         {
-            drawCircle(x, y, 40 + iter * 3, 1);
+            drawCircle(x, y, 20 + iter * 2, 1);
         }
         else
         {
-            drawRect(x, y, 50 + iter * 3, 50 + iter * 3, 2);
+            drawRect(x, y, 50 + iter * 2, 50 + iter * 2, 2);
         }
 
         for (i = 0; i < 10000000; i++)
@@ -29,9 +29,6 @@ int main()
         }
 
     } while (1);
-
-end:
-    goto end;
 
 end:
     goto end;
