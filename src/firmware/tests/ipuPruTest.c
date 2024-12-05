bd089f41b86b7e3631fc6d30e59efb171b321f5c
Rusheel <rusheel@caredocs.ai>
2024-12-05 17:16:21 -0600

    ipu pru cursor test hex

---
+++ src/firmware/tests/ipuPruTest.c
@@ -0,0 +1,18 @@
+#include "utils.h"
+
+int main()
+{
+    int *x;
+    int *y;
+
+    while (1)
+    {
+        drawRect(0, 0, 640, 480, 0);
+        getCursorLocation(x, y);
+        drawCircle(*x, *y, 20, 3);
+    }
+
+end:
+    goto end;
+    return 0;
+}
