dfbc888217ff98b49af36f0848f67a53e1e8170a
lmyszkowski <lmyszkowski@ad.engr.wisc.edu>
2024-12-08 12:52:02 -0600

    updated test to work

--- src/firmware/tests/ipuPruTest.c
+++ src/firmware/tests/ipuPruTest.c
@@ -2,16 +2,24 @@
 
 int main()
 {
-    int *x;
-    int *y;
-    bool *present;
-    bool *valid;
-
     while (1)
     {
-        drawRect(0, 0, 640, 480, 0);
-        getCursorLocation(x, y, present, valid);
-        drawCircle(*x, *y, 20, 3);
+        int loc = getCursorLocation();
+        int sh1 = loc >> 1;
+        int sh2 = loc >> 2;
+        int sh3 = loc >> 12;
+
+        bool present = sh1 & 1;
+        bool valid = loc & 1;
+
+        int rev_x = sh2 & 0x3FF;
+        int y = sh3 & 0x1FF;
+        int x = 640 - rev_x;
+
+        if (present && valid) {
+            drawRect(0, 0, 640, 480, 0);
+            drawCircle(x, y, 20, 3);
+        }
     }
 
 end:
