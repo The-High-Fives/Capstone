3b14bc19347f9f9420f2c8bfc46bca3b56532fb2
Rusheel <rusheel@caredocs.ai>
2024-12-07 20:51:17 -0600

    ipuPruTest

--- src/firmware/tests/ipuPruTest.c
+++ src/firmware/tests/ipuPruTest.c
@@ -4,11 +4,13 @@ int main()
 {
     int *x;
     int *y;
+    int *present;
+    int *valid;
 
     while (1)
     {
         drawRect(0, 0, 640, 480, 0);
-        getCursorLocation(x, y);
+        getCursorLocation(x, y, present, valid);
         drawCircle(*x, *y, 20, 3);
     }
 
