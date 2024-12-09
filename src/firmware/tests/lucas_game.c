8a85ed4495e985827e5d0e037e3beb8dd8ee1977
lmyszko <119759329+lmyszko@users.noreply.github.com>
2024-12-09 13:09:40 -0600

    Update lucas_game.c with score

--- src/firmware/tests/lucas_game.c
+++ src/firmware/tests/lucas_game.c
@@ -19,6 +19,14 @@
 #define SPRITE_CODE 0b10
 #define LETTER_CODE 0b11
 
+#define LETTER_BASE 0
+#define LETTER_OFFSET 512
+
+#define EXCLAMATION 18432
+#define QUESTION 18944
+#define COLON 19456
+#define HYPHEN 19968
+
 #define color_t unsigned char
 #define bool unsigned char
 #define uint unsigned int
@@ -37,33 +45,30 @@ typedef struct
 void drawCircle(int x, int y, int radius, color_t color);
 void drawRect(int x, int y, int width, int height, color_t color);
 void drawSprite(int x, int y, int scale, int addr, color_t color);
+void drawChar(int x, int y, char c, color_t color);
+void drawScore(int startX, int y, int score, color_t color);
 void drawLetter(int x, int y, int scale, int addr, color_t color);
-void setColor(color_t addr, int r, int g, int b);
+void setColor(color_t addr, Color color);
 void setLED(bool value, int led, int *ledState);
 
 uint getTimerValue();
 bool checkLocationForColor(int x, int y, int radius);
-// void getCursorLocation(int *x, int *y, bool *present, bool *valid);
 int getCursorLocation();
 char getSPART();
 void setSPART(char value);
 void getIO(int *timer, char *SPART, int *x, int *y, bool *present, bool *valid);
 
 // ================================================================================================
-
 #define CURSOR_RADIUS 5
 #define CIRCLE_RADIUS 20
-
 int main()
 {
     int loc;
     int sh1;
     int sh2;
     int sh3;
-
     int prev_x = 0;
     int prev_y = 0;
-
     int x = 0;
     int rev_x;
     int y = 0;
@@ -78,74 +83,124 @@ int main()
     int game_y_loc;
     int game_x_upper;
     int game_y_upper;
-    drawRect(0, 0, 640, 480, 0);
-
+    uint panic = 0;
+    int no_hit = 0;
+    
+    // Color c2 = {0xF0F, 0xFFF, 0x0F0};
+    // setColor(2, c2);
+     drawRect(0, 0, 640, 480, 0);
+    // drawScore(50, 300, 5, 2);
+    // //drawScore(10, 200, 7, 2);
+        // if (x > 100 && x < 140 && y > 100 && y < 140)
+        // {
+        //         Color c2 = {0xF0F, 0xFFF, 0x0F0};
+        //         setColor(3, c2);
+        //     // drawCircle(120, 120, 20, 0);
+        //     // //hit = hit + 1;
+        //     // drawCircle(200, 200, 10, 2);
+        // }
+        // else
+        // {
+        //         Color c3 = {0x00F, 0x00F, 0x0FF};
+        //         setColor(3, c3);
+        //     // drawCircle(120, 120, 20, 1);
+        //     // //hit = hit + 1;
+        //     // drawCircle(200, 200, 10, 0);
+        // }
     while (1)
     {
         loc = getCursorLocation();
         sh1 = loc >> 1;
         sh2 = loc >> 2;
         sh3 = loc >> 12;
-
+        //panic = getTimerValue();
+        //panic = panic >> 8;
         present = sh1 & 1;
         valid = loc & 1;
-
         rect_x = prev_x - 24;
         rect_y = prev_y - 24;
-
         rev_x = sh2 & 0x3FF;
         x = 640 - rev_x;
         y = sh3 & 0x1FF;
 
-        game_x_lower = 40 + hit * 50;
-        game_y_lower = 40 + hit * 20;
-        game_x_upper = 100 + hit * 50;
-        game_y_upper = 100 + hit * 20;
-        game_x_loc = 60 + hit * 50;
-        game_y_loc = 60 + hit * 20;
+        // game_x_lower = 40 + hit << 5;
+        // game_y_lower = 40 + hit << 3;
+        // game_x_upper = 70 + hit << 5;
+        // game_y_upper = 70 + hit << 3;
+        // game_x_loc = 60 + hit << 5;
+        // game_y_loc = 60 + hit << 3;
 
-        if (hit == 3)
-        {
-            hit = 0;
-        }
 
         if (present && valid)
         {
+        //  if (x > 100 && x < 140 && y > 100 && y < 140)
+        // {
+        //         Color c2 = {0xF0F, 0xFFF, 0x0F0};
+        //         setColor(3, c2);
+        //     // drawCircle(120, 120, 20, 0);
+        //     // //hit = hit + 1;
+        //     // drawCircle(200, 200, 10, 2);
+        // }
+        // else
+        // {
+        //         Color c3 = {0x00F, 0x00F, 0x0FF};
+        //         setColor(3, c3);
+        //     // drawCircle(120, 120, 20, 1);
+        //     // //hit = hit + 1;
+        //     // drawCircle(200, 200, 10, 0);
+        // }
 
-            if (x > 40 && x < 100 && y > 40 && y < 100)
-            {
-                drawCircle(60, 60, 20, 0);
-                hit = hit + 1;
-                drawCircle(200, 200, 10, 2);
-            }
-            else
-            {
-                // drawRect(, game_y_loc, 30, 30, 2);
-                drawRect(game_x_loc, game_y_loc, 40, 40, 1);
-            }
+        game_x_lower = 60 + (hit);
+        game_y_lower = 60 + (hit);
+        game_x_upper = 140 + (hit);
+        game_y_upper = 140 + (hit);
+        game_x_loc = 90 + (hit);
+        game_y_loc = 90 + (hit);
+        // if (!(hit ^ 4))
+        // {
+        //     hit = 0;
+        // }
+        if (x > game_x_lower && x < game_x_upper && y > game_y_lower && y < game_y_upper)
+        {
+                Color c2 = {0xF0F, 0xFFF, hit};
+                setColor(3, c2);
+             drawCircle(game_x_loc, game_y_loc, 20, 0);
+             hit = hit + 20;
+             //drawScore(50, 300, hit, 2);
+            //drawCircle(200, 200, 10, 2);
+        }
+        else
+        {
+                no_hit = no_hit + 39;
+                if (no_hit == 7098) {
+                    no_hit = 0;
+                }
+                Color game_circle = {no_hit, no_hit >> 2, 0x0F0};
+                setColor(1, game_circle);
+            //drawCircle(200, 200, 10, 0);
+             drawCircle(game_x_loc, game_y_loc, 20, 1);
+        }
             prev_x = x;
             prev_y = y;
-
+            drawScore(1, 1, hit, 2);
+            //drawScore(5, 65, panic, 2);
             drawRect(rect_x, rect_y, 48, 48, 0);
             drawCircle(x, y, 20, 3);
         }
     }
-
 end:
     goto end;
     return 0;
 }
 
-// ================================================================================================
-
-void setColor(color_t addr, int r, int g, int b)
+void setColor(color_t addr, Color color)
 {
     int *memSet;
     int command;
 
     memSet = (int *)(COLOR_ADDR + (addr & 3) * 4);
 
-    command = ((r & 0x3FF) << 20) | ((g & 0x3FF) << 10) | (b & 0x3FF);
+    command = ((color.r & 0x3FF) << 20) | ((color.g & 0x3FF) << 10) | (color.b & 0x3FF);
     *memSet = command;
 }
 
@@ -190,9 +245,101 @@ void drawSprite(int x, int y, int scale, int addr, color_t color)
     command = addr;
     *memSet = command;
 
-    memSet = (int *)DRAW_CONTROL_ADDR;
-    command = (1 << 31) | (scale & 0x7FFFF);
-    *memSet = command;
+    // memSet = (int *)DRAW_CONTROL_ADDR;
+    // command = (1 << 31) | (scale & 0x7FFFF);
+    // *memSet = command;
+}
+
+void drawScore(int startX, int y, int score, color_t color)
+{
+    if (score > 0xFFF)
+    {
+        score = 0xFFF;
+    }
+
+    int hundreds = (score >> 8) & 0xF;
+    int tens = (score >> 4) & 0xF;
+    int ones = score & 0xF;
+
+    int xC = startX + 16;
+    int xO = startX + 32;
+    int xR = startX + 48;
+    int xE = startX + 64;
+    int xColon = startX + 80;
+    int xHundred = startX + 112;
+    int xTen = startX + 128;
+    int xOne = startX + 144;
+    drawRect(startX, y, (xOne + 24), 38, 0);
+    char hundred;
+    char ten;
+    char one;
+
+    if (hundreds > 9)
+    {
+        hundred = 'a' + (hundreds - 10);
+    }
+    else
+    {
+        hundred = '0' + hundreds;
+    }
+
+    if (tens > 9)
+    {
+        ten = 'a' + (tens - 10);
+    }
+    else
+    {
+        ten = '0' + tens;
+    }
+
+    if (ones > 9)
+    {
+        one = 'a' + (ones - 10);
+    }
+    else
+    {
+        one = '0' + ones;
+    }
+
+    drawChar(startX, y, 's', color);
+    drawChar(xC, y, 'c', color);
+    drawChar(xO, y, 'o', color);
+    drawChar(xR, y, 'r', color);
+    drawChar(xE, y, 'e', color);
+    drawChar(xColon, y, ':', color);
+    drawChar(xHundred, y, hundred, color);
+    drawChar(xTen, y, ten, color);
+    drawChar(xOne, y, one, color);
+}
+
+void drawChar(int x, int y, char c, color_t color)
+{
+    if (c == '!')
+    {
+        drawLetter(x, y, 1, EXCLAMATION, color);
+    }
+    else if (c == '?')
+    {
+        drawLetter(x, y, 1, QUESTION, color);
+    }
+    else if (c == ':')
+    {
+        drawLetter(x, y, 1, COLON, color);
+    }
+    else if (c == '-')
+    {
+        drawLetter(x, y, 1, HYPHEN, color);
+    }
+    else if (c >= '0' && c <= ':')
+    { // Digits
+        int addr = LETTER_BASE + LETTER_OFFSET * (26 + (c - '0'));
+        drawLetter(x, y, 1, addr, color);
+    }
+    else
+    { // Letters
+        int addr = LETTER_BASE + LETTER_OFFSET * (c - 'a');
+        drawLetter(x, y, 1, addr, color);
+    }
 }
 
 void drawLetter(int x, int y, int scale, int addr, color_t color)
@@ -208,9 +355,9 @@ void drawLetter(int x, int y, int scale, int addr, color_t color)
     command = addr;
     *memSet = command;
 
-    memSet = (int *)DRAW_CONTROL_ADDR;
-    command = (1 << 31) | (scale & 0x7FFFF);
-    *memSet = command;
+    // memSet = (int *)DRAW_CONTROL_ADDR;
+    // command = (1 << 31) | (scale & 0x7FFFF);
+    // *memSet = command;
 }
 
 void setLED(bool value, int led, int *ledState)
@@ -263,26 +410,26 @@ int getCursorLocation()
     return *memSet;
 }
 
-char getSPART()
-{
-    char *memSet;
+// char getSPART()
+// {
+//     char *memSet;
 
-    memSet = (char *)SPART_READ_ADDR;
-    return *memSet;
-}
+//     memSet = (char *)SPART_READ_ADDR;
+//     return *memSet;
+// }
 
-void setSPART(char value)
-{
-    int *memSet;
-    int command;
+// void setSPART(char value)
+// {
+//     int *memSet;
+//     int command;
 
-    memSet = (int *)SPART_WRITE_ADDR;
-    command = (value & 0xFF);
-    *memSet = command;
-}
+//     memSet = (int *)SPART_WRITE_ADDR;
+//     command = (value & 0xFF);
+//     *memSet = command;
+// }
 
-void getIO(int *timer, char *SPART, int *x, int *y, bool *present, bool *valid)
-{
-    *timer = getTimerValue();
-    *SPART = getSPART();
-}
+// void getIO(int *timer, char *SPART, int *x, int *y, bool *present, bool *valid)
+// {
+//     *timer = getTimerValue();
+//     *SPART = getSPART();
+// }
